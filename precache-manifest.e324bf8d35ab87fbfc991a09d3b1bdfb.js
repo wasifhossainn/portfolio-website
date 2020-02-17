ccf97a016d23c7b2ac4681d39e9a134b124764f4
Wasif Hossain <wasif.hossain@hotmail.com>
2020-02-17 17:26:28 +1100

    Updates

---
+++ precache-manifest.e324bf8d35ab87fbfc991a09d3b1bdfb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908362b8e72be717f381ce24844feb63",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "4c4b8f1d8c8b1ce377da",
+    "url": "/portfolio-website/static/css/main.bb597055.chunk.css"
+  },
+  {
+    "revision": "f1bedfd0b2dc65d63422",
+    "url": "/portfolio-website/static/js/2.da659a5f.chunk.js"
+  },
+  {
+    "revision": "209d7f261911842216918882d1f3ddbc",
+    "url": "/portfolio-website/static/js/2.da659a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4b8f1d8c8b1ce377da",
+    "url": "/portfolio-website/static/js/main.3e00b4bf.chunk.js"
+  },
+  {
+    "revision": "8602db4a7a04e93fc5d1",
+    "url": "/portfolio-website/static/js/runtime-main.087a6ff3.js"
+  },
+  {
+    "revision": "35be897ddc97f36b052d26791aea8a69",
+    "url": "/portfolio-website/static/media/Algorithms.35be897d.svg"
+  },
+  {
+    "revision": "8708778ec2cae15b853407318fe3b0a3",
+    "url": "/portfolio-website/static/media/ChatBubble.8708778e.svg"
+  },
+  {
+    "revision": "e5649e219a17c1d0e4abf3d20490d110",
+    "url": "/portfolio-website/static/media/Divider2.e5649e21.svg"
+  },
+  {
+    "revision": "d3268f88d73ff0d2456a8f8c98b808f4",
+    "url": "/portfolio-website/static/media/EmailIcon.d3268f88.svg"
+  },
+  {
+    "revision": "76793e2d7c0edbe772a8db8f6c3074c3",
+    "url": "/portfolio-website/static/media/Evernoteapp.76793e2d.svg"
+  },
+  {
+    "revision": "2cc188266ef92862e1e7f9e37a4f5bde",
+    "url": "/portfolio-website/static/media/GitIcon.2cc18826.svg"
+  },
+  {
+    "revision": "70c145354e48cabd29ef75a6cc5911cd",
+    "url": "/portfolio-website/static/media/LinkedInIcon.70c14535.svg"
+  },
+  {
+    "revision": "a8bfda60d69a1789366c3226531b69ec",
+    "url": "/portfolio-website/static/media/Monitor.a8bfda60.svg"
+  },
+  {
+    "revision": "cd64e613cbb8989738d6db8bfe20c05b",
+    "url": "/portfolio-website/static/media/OneVice.cd64e613.svg"
+  },
+  {
+    "revision": "9570a5ef9e3902a87aae25fd7753e6d4",
+    "url": "/portfolio-website/static/media/OutrunfutureBoldItalic.9570a5ef.otf"
+  },
+  {
+    "revision": "92342e460dad424eceda832c914e1763",
+    "url": "/portfolio-website/static/media/ResumeIcon.92342e46.svg"
+  },
+  {
+    "revision": "db818fea7a4bc76997b2148d1ed97a5a",
+    "url": "/portfolio-website/static/media/SpaceInvaders.db818fea.svg"
+  },
+  {
+    "revision": "1d00fbc3d27633893b615ce0c09e79b6",
+    "url": "/portfolio-website/static/media/StreamApp.1d00fbc3.svg"
+  },
+  {
+    "revision": "ad69bc3d2c75c8b433db3462be01d211",
+    "url": "/portfolio-website/static/media/StudentWorkingComp.ad69bc3d.svg"
+  },
+  {
+    "revision": "c17eb1296a032a2b2e55bad64665ac34",
+    "url": "/portfolio-website/static/media/Team.c17eb129.svg"
+  },
+  {
+    "revision": "4abf9cd633cbde9ab5f50bcb2bf067b5",
+    "url": "/portfolio-website/static/media/TechBoy.4abf9cd6.svg"
+  },
+  {
+    "revision": "bfea57f9ab9200b5f4169dd46a60bff2",
+    "url": "/portfolio-website/static/media/WH Logo.bfea57f9.png"
+  },
+  {
+    "revision": "5871b581a08ce3c01d19ac251337793f",
+    "url": "/portfolio-website/static/media/WHPortfolio.5871b581.svg"
+  },
+  {
+    "revision": "9693de19f1ae3587b140d93973956daf",
+    "url": "/portfolio-website/static/media/Wasif_Hossain_Resume.9693de19.pdf"
+  },
+  {
+    "revision": "5b04cca285e6b155c6d73406e40ed5a5",
+    "url": "/portfolio-website/static/media/background.5b04cca2.svg"
+  },
+  {
+    "revision": "84dec6467aa039dea880278634e4bc37",
+    "url": "/portfolio-website/static/media/carGame.84dec646.svg"
+  },
+  {
+    "revision": "e56549fcaefa65e7fe554b9bad5989ab",
+    "url": "/portfolio-website/static/media/ezyDonate-promo-website.e56549fc.svg"
+  },
+  {
+    "revision": "3a28f8fd0066a857c982d8a770b9d4af",
+    "url": "/portfolio-website/static/media/ezyDonate.3a28f8fd.svg"
+  },
+  {
+    "revision": "c735b61ec42683e4fb046e6093cf6270",
+    "url": "/portfolio-website/static/media/menu-color-filled.c735b61e.svg"
+  }
+]);
