db58d9d715d6014f301236c639ac2ac8d1cba436
Wasif Hossain <wasif.hossain@hotmail.com>
2020-07-14 13:16:03 +1000

    UPDATED AND FIXED

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-scroll';
-
 import SideBar from '../common/SideBar';
 import Logo from '../../assets/WH Logo.png';
 import navIcon from '../../assets/menu-color-filled.svg';
@@ -10,105 +9,151 @@ import './style.scss';
 import classnames from 'classnames';
 
 class Navigation extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			close: true,
-			prevScrollpos: window.pageYOffset,
-			visible: true
-		};
-		this.menuButton = this.menuButton.bind(this);
-	}
+    this.state = {
+      close: true,
+      prevScrollpos: window.pageYOffset,
+      visible: true,
+    };
+    this.menuButton = this.menuButton.bind(this);
+  }
 
-	// Open SideBar
-	menuButton = () => {
-		const { close } = this.state;
-		this.setState({
-			close: !close
-		});
-	};
+  // Open SideBar
+  menuButton = () => {
+    const { close } = this.state;
+    this.setState({
+      close: !close,
+    });
+  };
 
-	// Adds an event listener when the component is mount
-	componentDidMount() {
-		window.addEventListener('scroll', this.handleScroll);
-	}
+  // Adds an event listener when the component is mount
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
 
-	// Remove the event listener when the component is unmount
-	componentWillUnmount() {
-		window.removeEventListener('scroll', this.handleScroll);
-	}
+  // Remove the event listener when the component is unmount
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 
-	// Hide or show menu
-	handleScroll = () => {
-		const { prevScrollpos } = this.state;
+  // Hide or show menu
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
 
-		const currentScrollPos = window.pageYOffset;
-		const visible = prevScrollpos > currentScrollPos;
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollpos > currentScrollPos;
 
-		this.setState({
-			prevScrollpos: currentScrollPos,
-			visible
-		});
-	};
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible,
+    });
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				<nav
-					className={classnames('navbar', {
-						'navbar--hidden': !this.state.visible
-					})}
-				>
-					<Link activeClass="active" to="Footer" spy={true} smooth={true} offset={-70} duration={500}>
-						<img src={Logo} title="Footer" alt="" className="logo" />
-					</Link>
-					<div className="navbar-buttons">
-						<ul>
-							<li>
-								<Link activeClass="active" to="Home" spy={true} smooth={true} offset={70} duration={500}>
-									<button className="homeBtn">Home</button>
-								</Link>
-							</li>
+  render() {
+    return (
+      <React.Fragment>
+        <nav
+          className={classnames('navbar', {
+            'navbar--hidden': !this.state.visible,
+          })}
+        >
+          <Link
+            activeClass='active'
+            to='Footer'
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            <img src={Logo} title='Footer' alt='' className='logo' />
+          </Link>
+          <div className='navbar-buttons'>
+            <ul>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Home'
+                  spy={true}
+                  smooth={true}
+                  offset={70}
+                  duration={500}
+                >
+                  <button className='homeBtn'>Home</button>
+                </Link>
+              </li>
 
-							<li>
-								<Link activeClass="active" to="About" spy={true} smooth={true} offset={50} duration={500}>
-									<button className="aboutBtn">About</button>
-								</Link>
-							</li>
-							<li>
-								<Link activeClass="active" to="Contact" spy={true} smooth={true} offset={50} duration={500}>
-									<button className="contactBtn">Contact</button>
-								</Link>
-							</li>
-							<li>
-								<Link activeClass="active" to="Project" spy={true} smooth={true} offset={0} duration={500}>
-									<button className="projectBtn">Projects</button>
-								</Link>
-							</li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='About'
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  <button className='aboutBtn'>About</button>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Contact'
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                >
+                  <button className='contactBtn'>Contact</button>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Project'
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                >
+                  <button className='projectBtn'>Projects</button>
+                </Link>
+              </li>
 
-							<li>
-								<Link activeClass="active" to="Skills" spy={true} smooth={true} offset={230} duration={500}>
-									<button className="skillBtn">Skills</button>
-								</Link>
-							</li>
-						</ul>
-					</div>
-					<div onClick={this.menuButton} className="menu-toggle">
-						<img className="menu" src={!this.state.close ? navIcon : crossIcon} alt="/" />
-					</div>
-				</nav>
+              <li>
+                <Link
+                  activeClass='active'
+                  to='Skills'
+                  spy={true}
+                  smooth={true}
+                  offset={230}
+                  duration={500}
+                >
+                  <button className='skillBtn'>Skills</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div onClick={this.menuButton} className='menu-toggle'>
+            <img
+              className='menu'
+              src={!this.state.close ? navIcon : crossIcon}
+              alt='/'
+            />
+          </div>
+        </nav>
 
-				<div
-					className={classnames('sidebar--hidden', {
-						sidebar: this.state.close && this.state.visible
-					})}
-				>
-					<SideBar />
-				</div>
-			</React.Fragment>
-		);
-	}
+        <div
+          className={classnames('sidebar--hidden', {
+            sidebar: this.state.close && this.state.visible,
+          })}
+        >
+          <SideBar />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Navigation;
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -8,41 +8,74 @@ import CSS3 from '../../assets/css3-original.svg';
 import Javascript from '../../assets/javascript-original.svg';
 import ReactIcon from '../../assets/react-original.svg';
 import ReduxIcon from '../../assets/redux-original.svg';
+import MongoDBIcon from '../../assets/mongodb.svg';
+import ExpressIcon from '../../assets/express-109.svg';
+import NodeJSIcon from '../../assets/nodejs-1.svg';
 import Python from '../../assets/python-original.svg';
 import Java from '../../assets/java-original.svg';
 import SQL from '../../assets/mysql-original.svg';
 import C from '../../assets/c-original.svg';
+import Android from '../../assets/android.svg';
 
 import './style.scss';
 
 const Skills = () => {
-	return (
-		<React.Fragment>
-			<div className="skill-content" id="Skills">
-				<img alt="" className="divider" src={Divider} />
-				<img alt="" className="team" src={Team} />
-				<h1 className="skillHeading">TECHNICAL SKILLS</h1>
-				<p className="skill-section-1">
-					Recently, I worked at Stomble (June 2019- January 2020) as a front-end developer in a <br /> SCRUM environment
-					where we created a video platform using ReactJS, AWS APIs, <br /> Material UI and Redux Sagas. Also, used
-					tools such as GitLab for pipeline testing and <br /> ensuring the application would not encounter run-time
-					error and Trello for organising <br /> SCRUM product backlog. The application connects businesses and users,
-					allowing users <br /> to absorb content through videos that are posted by businesses.
-				</p>
-				<div className="skill-icon-container">
-					<img alt="" title="HTML5" className="skill-icon" src={HTML5} />
-					<img alt="" title="CSS3" className="skill-icon" src={CSS3} />
-					<img alt="" title="Vanilla Javascript" className="skill-icon" src={Javascript} />
-					<img alt="" title="React" className="skill-icon" src={ReactIcon} />
-					<img alt="" title="Redux" className="skill-icon" src={ReduxIcon} />
-					<img alt="" title="Python" className="skill-icon" src={Python} />
-					<img alt="" title="Java" className="skill-icon" src={Java} />
-					<img alt="" title="SQL" className="skill-icon" src={SQL} />
-					<img alt="" title="C" className="skill-icon" src={C} />
-				</div>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div className='skill-content' id='Skills'>
+        <img alt='' className='divider' src={Divider} />
+        <img alt='' className='team' src={Team} />
+        <h1 className='skillHeading'>TECHNICAL SKILLS</h1>
+        <p className='skill-section-1'>
+          Recently, I worked at Stomble (June 2019- February 2020) as a
+          front-end developer in a <br /> SCRUM environment where we created a
+          video platform using ReactJS, AWS APIs, <br /> Material UI and Redux
+          Sagas. Also, used tools such as GitLab for pipeline testing and <br />{' '}
+          ensuring the application would not encounter run-time error and Trello
+          for organising <br /> SCRUM product backlog. The application connects
+          businesses and users, allowing users <br /> to absorb content through
+          videos that are posted by businesses.
+        </p>
+        <div className='skill-icon-container'>
+          <img alt='' title='HTML5' className='skill-icon' src={HTML5} />
+          <img alt='' title='CSS3' className='skill-icon' src={CSS3} />
+          <img
+            alt=''
+            title='Vanilla Javascript'
+            className='skill-icon'
+            src={Javascript}
+          />
+          <img
+            alt=''
+            title='MonogoDB'
+            className='skill-icon'
+            src={MongoDBIcon}
+          />
+          <img
+            alt=''
+            title='Express'
+            className='skill-icon'
+            src={ExpressIcon}
+          />
+          <img alt='' title='React' className='skill-icon' src={ReactIcon} />
+          <img alt='' title='Redux' className='skill-icon' src={ReduxIcon} />
+          <img alt='' title='Node.JS' className='skill-icon' src={NodeJSIcon} />
+          <img alt='' title='Python' className='skill-icon' src={Python} />
+        </div>
+        <div className='skill-icon-container'>
+          <img alt='' title='Java' className='skill-icon' src={Java} />
+          <img alt='' title='SQL' className='skill-icon' src={SQL} />
+          <img alt='' title='C' className='skill-icon' src={C} />
+          <img
+            alt=''
+            title='Android Development'
+            className='skill-icon'
+            src={Android}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Skills;
