46ba047c8d184235eb55651fde3ebe08ca44f969
Wasif Hossain <wasif.hossain@hotmail.com>
2020-07-14 13:27:36 +1000

    PATCHED SECURITY FIX

--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -10,40 +10,40 @@ import Lottie from 'react-lottie';
 import './style.scss';
 
 const Projects = () => {
-	return (
-		<React.Fragment>
-			<div className="projects-content" id="Project">
-				<img alt="" className="divider" src={Divider} />
+  return (
+    <React.Fragment>
+      <div className='projects-content' id='Project'>
+        <img alt='' className='divider' src={Divider} />
 
-				<div className="project-wrapper">
-					<h1 className="projectHeading">PROJECTS</h1>
+        <div className='project-wrapper'>
+          <h1 className='projectHeading'>PROJECTS</h1>
 
-					<img alt="" className="codingboy" src={CodingBoy} />
-				</div>
+          <img alt='' className='codingboy' src={CodingBoy} />
+        </div>
 
-				<div className="monitor-wrapper">
-					<img alt="" className="monitor" src={Monitor} />
-					<div className="slider-scale">
-						<Slider />
-					</div>
-				</div>
-				<div className="task-animation">
-					<Lottie
-						options={{
-							loop: true,
-							autoplay: true,
-							animationData: Task,
-							rendererSettings: {
-								preserveAspectRatio: 'xMidYMid slice'
-							}
-						}}
-						width={400}
-						height={400}
-					/>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+        <div className='monitor-wrapper'>
+          <img alt='' className='monitor' src={Monitor} />
+          <div className='slider-scale'>
+            <Slider />
+          </div>
+        </div>
+        <div className='task-animation'>
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: Task,
+              rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice',
+              },
+            }}
+            width={400}
+            height={400}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Projects;
