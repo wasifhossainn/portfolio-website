3cd4fe03120232128fb10892e920d9abaf322cc9
Wasif Hossain <wasif.hossain@hotmail.com>
2020-07-15 12:13:09 +1000

    UPDATED RESUME

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,33 +10,48 @@ import PDF from '../../assets/Wasif-Hossain-Resume.pdf';
 import './style.scss';
 
 const Footer = () => {
-	return (
-		<React.Fragment>
-			<div className="footer" id="Footer">
-				<img alt="" className="divider" src={Divider} />
+  return (
+    <React.Fragment>
+      <div className='footer' id='Footer'>
+        <img alt='' className='divider' src={Divider} />
 
-				<div className="footer-bar-container">
-					<a rel="noopener noreferrer" alt="" target="_blank" href="https://github.com/wasifhossainn">
-						<img className="footer-item" alt="" src={Github} />
-					</a>
-					<a alt="" rel="noopener noreferrer" target="_blank" href="https://au.linkedin.com/in/wasif-hossain-b4398115a">
-						<img className="footer-item" alt="" src={LinkedIn} />
-					</a>
-					<a alt="" rel="noopener noreferrer" target="_blank" href="mailto:wasif.hossain@hotmail.com">
-						<img className="footer-item" alt="" src={Email} />
-					</a>
-					<a alt="" rel="noopener noreferrer" target="_blank" href={PDF}>
-						<img className="footer-item" alt="" src={Resume} />
-					</a>
-				</div>
+        <div className='footer-bar-container'>
+          <a
+            rel='noopener noreferrer'
+            alt=''
+            target='_blank'
+            href='https://github.com/wasifhossainn'
+          >
+            <img className='footer-item' alt='' src={Github} />
+          </a>
+          <a
+            alt=''
+            rel='noopener noreferrer'
+            target='_blank'
+            href='https://au.linkedin.com/in/wasif-hossain-b4398115a'
+          >
+            <img className='footer-item' alt='' src={LinkedIn} />
+          </a>
+          <a
+            alt=''
+            rel='noopener noreferrer'
+            target='_blank'
+            href='mailto:wasif.hossain@hotmail.com'
+          >
+            <img className='footer-item' alt='' src={Email} />
+          </a>
+          <a alt='' rel='noopener noreferrer' target='_blank' href={PDF}>
+            <img className='footer-item' alt='' src={Resume} />
+          </a>
+        </div>
 
-				<div className="footer-text">
-					<p>Copyright © Wasif Hossain</p>
-					<p> Credit to Ameli Lin for Background Image</p>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+        <div className='footer-text'>
+          <p>Copyright © Wasif Hossain</p>
+          <p> Credit to Ameli Lin for Background Image</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Footer;
